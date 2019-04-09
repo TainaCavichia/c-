b787081dc1ce73e4b8ca815b0816241c60175558
Tainá Cavichia Alves <46748080819@info.local>
2019-04-09 11:30:45 -0300

    b

---
+++ aulas C#/Csharp/airline/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace airline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repetir = true;
+
+            while (repetir)
+            {
+
+            Console.WriteLine("----------Escolha uma opção----------");   
+            Console.WriteLine("1 - Registrar Passagem");
+            Console.WriteLine("2 - Visualizar Passagens");
+            Console.WriteLine("0 - Sair");
+            int resposta = int.Parse(Console.ReadLine());
+
+            switch (resposta)
+            {
+                case 1:
+
+                for (int i = 0; i < 5; i++)
+                {     
+                Console.WriteLine("Digite o seu nome completo");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Digite o número de passagens");
+                int numPassagem = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite a data do voo");
+                DateTime data = DateTime.Parse(Console.ReadLine());
+                }
+
+                break;
+                
+                case 2:
+                break;
+
+                case 0:
+                repetir = false;
+                break;
+            }//fim do switch
+            }//fim do while
+
+        }
+    }
+}
---
+++ aulas C#/Csharp/banco/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] notas = {100, 50, 20, 10, 5, 2, 1};
+
+            Console.WriteLine("Digite o quanto voce quer sacar:");
+            int saque = int.Parse(Console.ReadLine());
+            DateTime data = DateTime.Now;
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+               int numeroDeNotas = saque / notas[i];
+               int restante = saque % notas[i];
+               saque = restante;
+
+               if (numeroDeNotas != 0)
+               {
+                   Console.WriteLine($"Voce recebeu {numeroDeNotas} nota(s) de {notas[i]} em {data}");
+               }
+            }
+        }
+    }
+}
