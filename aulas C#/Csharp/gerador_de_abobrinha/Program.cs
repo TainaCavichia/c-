54406b23bdbfff097566b4588544491566dce8c4
Tainá Cavichia Alves <46748080819@info.local>
2019-04-11 11:22:13 -0300

    11

--- aulas C#/Csharp/gerador_de_abobrinha/Program.cs
+++ aulas C#/Csharp/gerador_de_abobrinha/Program.cs
@@ -9,7 +9,7 @@ namespace gerador_de_abobrinha
             Console.WriteLine("Escreve uma palavra ai, meu consagrado!");
 
             int maxPalavrasUsuario = 5;
-            // int maxPalavrasFrase = 8;
+            int maxPalavrasFrase = 8;
 
             string[] PalavrasUsuario = new string[maxPalavrasUsuario];
 
@@ -19,12 +19,41 @@ namespace gerador_de_abobrinha
                 if (--maxPalavrasUsuario != 0)
                 {
                     Console.WriteLine($"Faltam {maxPalavrasUsuario}. Digite mais uma");
-                }else
+                }
+                else
                 {
                     Console.WriteLine("Valeu,jovem! Agora deixa comigo");
                 }
             }
+            string[,] matrizPalavras = {
+                                   {"é","","jaca","dedo",""},
+                                   {"","janela","lixo","carro","piramide"},
+                                   {"coração","pedra","batata","","néctar"},
+                                   {"feijao","argola","carandiru","eu","virgula"},
+                                   {"rodizio","perola","sou","laranja",""}
+            };
 
+            for (int i = 0; i < matrizPalavras.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrizPalavras.GetLength(0); j++)
+                {
+                    if ("".Equals(matrizPalavras[i,j]))
+                    {
+                        matrizPalavras[i,j] = PalavrasUsuario[i];
+                    }
+                }
+            }
+
+            string frase = "";
+
+            Random r = new Random();
+            for (int i = 0; i < maxPalavrasFrase; i++)
+            {
+                frase += matrizPalavras[r.Next(matrizPalavras.GetLength(0)),
+                                        r.Next(matrizPalavras.GetLength(0))] 
+                                        + " ";
+            }
+            Console.WriteLine("Minha frase é: \n" + frase);
         }
     }
 }
---
+++ aulas C#/Csharp/notas/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando as médias");
+
+            string[] nome = new string[10];
+            string[] sobrenome = new string[10];
+            double[] notas1 = new double[10];
+            double[] notas2 = new double[10];
+            double[] soma = new double[10];
+            double[] media = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+
+            Console.WriteLine($"Digite o seu {i + 1}° nome:");
+            nome[i] = Console.ReadLine();
+
+            Console.WriteLine("Digite o seu sobrenome:");
+            sobrenome[i] = Console.ReadLine();
+
+            Console.WriteLine("Digite a primeira nota:");
+            notas1[i] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota:");
+            notas2[i] = double.Parse(Console.ReadLine());
+
+            soma[i] = notas1[i] + notas2[i]; 
+            media[i] = soma[i] / 2;
+
+            if (media[i] > 50)
+            {
+                Console.WriteLine($" Nome : {nome[i]} {sobrenome[i]} \n A média foi : {media[i]} \n O Aluno foi Aprovado");
+            }else
+            {
+                Console.WriteLine($" Nome : {nome[i]} {sobrenome[i]} \n A média foi : {media[i]} \n O Aluno foi Reprovado");
+            }
+
+            }
+
+        }
+    }
+}
